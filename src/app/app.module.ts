ca31c9f1d003573527fd48f39933ec905f5eb4c6
Sergio De La Hoz <sdelahoz@nimocom.com>
2020-10-18 13:18:26 -0500

    Adding devexpress table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { DxDataGridModule } from 'devextreme-angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, DxDataGridModule],
   providers: [],
   bootstrap: [AppComponent],
 })
