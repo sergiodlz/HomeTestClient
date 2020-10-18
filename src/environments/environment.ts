2bfa82e019790435458f6810de5443433e6a3239
Sergio De La Hoz <sdelahoz@nimocom.com>
2020-10-18 14:59:18 -0500

    Changing to ptoduction mode

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: 'http://sergio020893-001-site1.dtempurl.com/api',
 };
 
